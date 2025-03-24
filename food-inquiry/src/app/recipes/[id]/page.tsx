7172984e8c050dfacb495f6b38bd7f80e3e58891
nwadike ebere favour <favsamlol@gmail.com>
2025-03-24 11:27:52 +0100

    implementation and setup almost completed

--- food-inquiry/src/app/recipes/[id]/page.tsx
+++ food-inquiry/src/app/recipes/[id]/page.tsx
@@ -1,28 +1,3 @@
-// import { Box, Container, Flex } from "@chakra-ui/react"
-// import Link from "next/link"
-// import { ArrowLeft } from "lucide-react"
-// import RecipeDetail from "@/components/containers/recipe-detail";
-//
-//
-// export default function RecipePage({ params }: { params: { id: string } }) {
-//     return (
-//         <Box as="main" minH="100vh" bgGradient="linear(to-b, purple.50, white)" pt="4%">
-//             <Container maxW="container.xl" px={4} py={12}>
-//                 <Link href="/recipes" passHref>
-//                     <Flex as="a" align="center" color="purple.700" _hover={{ color: "purple.900" }} mb={8}>
-//                         <ArrowLeft size={16} style={{ marginRight: "8px" }} />
-//                         Back to recipes
-//                     </Flex>
-//                 </Link>
-//
-//                 <RecipeDetail id={params.id} />
-//             </Container>
-//         </Box>
-//     )
-// }
-//
-
-
 "use client";
 
 import { Box, Container, Flex } from "@chakra-ui/react";
